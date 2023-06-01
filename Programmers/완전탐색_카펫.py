6485ee73a5ccdf4484467965a7737a6d652fc17f
wda067 <mg6359@naver.com>
2023-06-01 19:22:59 +0900

    Update 완전탐색_카펫.py

--- Programmers/완전탐색_카펫.py
+++ Programmers/완전탐색_카펫.py
@@ -1,6 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42842
+# level2
+
 def solution(brown, yellow):
     total = brown + yellow
     for i in range(3, brown // 2):
         for j in range(3, brown // 2):
             if i >= j and total == i * j and (i - 2) * (j - 2) == yellow:
                 return [i, j]
+
+
+brown = 18
+yellow = 6
+print(solution(brown, yellow))
+# 정답 : [8, 3]
